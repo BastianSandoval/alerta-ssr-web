ec4a13407f43089e2b0663e7c8a7f27cef1a53ec
felipeinf <felipe.concha.a@mail.pucv.cl>
2021-01-29 12:03:38 -0300

    initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'admin',
+    pathMatch: 'full'
+  },
+  /*{
+    path: 'visitor',
+    loadChildren: () => import('./visitor/visitor.module')
+      .then(m => m.VisitorModule)
+  },*/
+  {
+    path: 'admin',
+    loadChildren: () => import('./admin/admin.module')
+      .then(m => m.AdminModule)
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
