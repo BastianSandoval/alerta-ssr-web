0c7aba2cc5e0e83b8c668adc5631a86df44227cc
David Godoy Ayala <godoyayaladavid@gmail.com>
2021-01-30 11:04:11 -0300

    feat(admin): admin-module

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -3,14 +3,21 @@ import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
 import { MaterialModule } from '@shared/modules/material/material.module';
+import { AddDogScreenComponent } from './screens/add-dog-screen/add-dog-screen.component';
+import { FormAddDogComponent } from './components/form-add-dog/form-add-dog.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
+const component = [
+  AdminComponent, AddDogScreenComponent, FormAddDogComponent
+]
 
 @NgModule({
-  declarations: [AdminComponent],
+  declarations: [...component],
   imports: [
     CommonModule,
     AdminRoutingModule,
-    MaterialModule
+    MaterialModule,
+    ReactiveFormsModule
   ]
 })
 export class AdminModule { }
