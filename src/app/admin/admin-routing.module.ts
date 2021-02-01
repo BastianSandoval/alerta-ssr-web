5d3c31e9036e93a530c1fe3fc68bb1b8a63ff0db
David Godoy Ayala <godoyayaladavid@gmail.com>
2021-02-01 10:25:14 -0300

    feat(admin): admin-routing

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AdminComponent } from './admin.component';
 import { AddDogScreenComponent } from './screens/add-dog-screen/add-dog-screen.component';
+import { ListDogsScreenComponent } from './screens/list-dogs-screen/list-dogs-screen.component';
 
 const routes: Routes = [
   {
@@ -12,6 +13,10 @@ const routes: Routes = [
         path: '',
         redirectTo: 'list-dogs',
         pathMatch: 'full'
+      },
+      {
+        path: 'list-dogs',
+        component: ListDogsScreenComponent
       },
       {
         path: 'add-dog',
