4c3ecb0d6b1b954e42eff1a42f7e70bcf50f801f
David Godoy Ayala <godoyayaladavid@gmail.com>
2021-02-04 12:10:33 -0300

    feat(core): breed provider

---
+++ src/app/core/providers/breed-provider/breed-provider.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpService } from '../../services/http/http.service';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BreedProviderService {
+
+  constructor(private httpService: HttpService) { }
+
+  public getBreed(): Promise<any[]> {
+    return this.httpService.get<any[]>('/breeds/list/all')
+      .pipe(map((data: any) => { 
+        let breeds = Object.keys(data);
+        return breeds;
+      })).toPromise();
+  } 
+}
