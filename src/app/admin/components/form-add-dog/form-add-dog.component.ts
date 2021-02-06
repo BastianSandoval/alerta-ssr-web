9495e40652798e78cdec8217fe4a4d1f45af59e8
Exequiel Andrade <exequiel.andrade16@gmail.com>
2021-02-06 10:40:56 -0300

    feat(admin): form add dog

--- src/app/admin/components/form-add-dog/form-add-dog.component.ts
+++ src/app/admin/components/form-add-dog/form-add-dog.component.ts
@@ -1,32 +1,66 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-
+import { Dog } from '@core/models/dog.model';
+import { BreedProviderService } from '@core/providers/breed-provider/breed-provider.service';
+import { DogService} from '../../../core/services/dogs/dogs.service';
 @Component({
   selector: 'app-form-add-dog',
   templateUrl: './form-add-dog.component.html',
   styleUrls: ['./form-add-dog.component.css']
 })
 export class FormAddDogComponent implements OnInit {
-
+  
   checkoutForm: FormGroup;
+  breeds?: string[];
+  image?: string;
 
-  constructor() {
+  constructor(
+    private breedProvider: BreedProviderService,
+    private dogService: DogService,
+    ){
     this.checkoutForm = this.createFormGroup();
-   }
+    }
 
-  ngOnInit(): void {
+   async ngOnInit(): Promise<void> {
+    try {
+      this.breeds = await this.breedProvider.getBreed();
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   private createFormGroup() {
     return new FormGroup({
-      nombrePerro: new FormControl('', [Validators.required]),
-      nombreDueno: new FormControl('',[Validators.required]),
-      raza: new FormControl('',[Validators.required]),
+      nameDog: new FormControl('', [Validators.required]),
+      nameOwner: new FormControl('',[Validators.required]),
+      breed: new FormControl('',[Validators.required]),
     })
   }
+  get nameDog() {return this.checkoutForm.get('nameDog');}
+  get nameOwner() {return this.checkoutForm.get('nameOwner');}
+  get breed() {return this.checkoutForm.get('breed');}
 
-  get nombrePerro() {return this.checkoutForm.get('nombrePerro')}
-  get nombreDueno() {return this.checkoutForm.get('nombreDueno')}
-  get raza() {return this.checkoutForm.get('raza')}
+  
+   public async addDog(){
+    await this.getImage()
+    let dog: Dog = {
+
+      _id: Math.random().toString(),
+      nameDog: this.checkoutForm.get('nameDog')?.value,
+      nameOwner: this.checkoutForm.get('nameOwner')?.value,
+      breed: this.checkoutForm.get('breed')?.value,
+      image: this.image!
+    };
+    this.dogService.addDog(dog)
+     // console.log(dog);
+    ;
+  }
+   async getImage(){
+    try {
+      this.image = await this.breedProvider.getBreedImage(this.checkoutForm.get('breed')?.value,);
+    } catch (error) {
+      console.log(error)
+     }
+  } 
 
 }
