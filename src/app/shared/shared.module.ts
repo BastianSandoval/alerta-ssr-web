a7a14a666ba6da076bcb416e9093e0a9d14e213f
David Godoy Ayala <godoyayaladavid@gmail.com>
2021-02-08 11:15:55 -0300

    feat(shared): shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,16 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BreedRepeatPipe } from './pipes/breed-repeat/breed-repeat.pipe';
 import { TableFilterPipe } from './pipes/table-filter/table-filter.pipe';
+import { DogFilterPipe } from './pipes/dog-filter/dog-filter.pipe';
 
 
 @NgModule({
-  declarations: [BreedRepeatPipe, TableFilterPipe],
+  declarations: [BreedRepeatPipe, TableFilterPipe, DogFilterPipe],
   imports: [
     CommonModule
   ],
   exports: [
     BreedRepeatPipe,
-    TableFilterPipe
+    TableFilterPipe,
+    DogFilterPipe
   ]
 })
 export class SharedModule { }
