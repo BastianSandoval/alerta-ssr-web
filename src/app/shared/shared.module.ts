de84f3c669113c048f328d06138dbedf3d92fc67
David Godoy Ayala <godoyayaladavid@gmail.com>
2021-02-08 11:19:56 -0300

    hotfix(shared): shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BreedRepeatPipe } from './pipes/breed-repeat/breed-repeat.pipe';
-import { TableFilterPipe } from './pipes/table-filter/table-filter.pipe';
+import { BreedFilterPipe } from './pipes/breed-filter/breed-filter.pipe';
 import { DogFilterPipe } from './pipes/dog-filter/dog-filter.pipe';
 
 
 @NgModule({
-  declarations: [BreedRepeatPipe, TableFilterPipe, DogFilterPipe],
+  declarations: [BreedRepeatPipe, BreedFilterPipe, DogFilterPipe],
   imports: [
     CommonModule
   ],
   exports: [
     BreedRepeatPipe,
-    TableFilterPipe,
+    BreedFilterPipe,
     DogFilterPipe
   ]
 })
