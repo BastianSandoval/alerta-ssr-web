c8ab158c2e0e6ed2de004eb84d0983a6d9906634
David Godoy Ayala <godoyayaladavid@gmail.com>
2021-02-17 12:23:31 -0300

    feat(admin):table dogs

--- src/app/admin/components/table-dogs/table-dogs.component.ts
+++ src/app/admin/components/table-dogs/table-dogs.component.ts
@@ -18,6 +18,13 @@ export class TableDogsComponent implements OnInit {
   filterDog!: string;
   idSelected: any;
   nameDogSelected: any;
+  dogsSlice!: Dog[];
+  sizePageTable: number = 7;
+  
+  startPage: number = 0;
+  endPage: number = 7;
+
+
 
   constructor(private breedProvider: BreedProviderService, private dogService: DogService) {
     this.dogs = dogService.dog;
@@ -33,6 +40,29 @@ export class TableDogsComponent implements OnInit {
       console.log(error)
     };
   }
+
+  ngDoCheck(){
+    this.dogsSlice = this.dogs.slice(this.startPage, this.endPage);
+
+    let prevButton = document.getElementById("prevButton");
+    let nextButton = document.getElementById("nextButton");
+
+    if (this.startPage === 0) {
+      prevButton?.setAttribute('disabled', 'disabled');
+
+    } else {
+      prevButton?.removeAttribute('disabled');
+      
+    }
+
+    if (this.endPage >= this.dogs.length) {
+      nextButton?.setAttribute('disabled', 'disabled');      
+    } else {
+      nextButton?.removeAttribute('disabled');
+    }
+    
+  }
+
 
   breedFilter(event:any) {
     this.filterBreed = event.target.value;
@@ -55,5 +85,21 @@ export class TableDogsComponent implements OnInit {
     this.filterDog = this.value;
   }
   
+  sizePage(event: any) {
+    this.sizePageTable = parseInt(event.target.value);
+    this.startPage = 0;
+    this.endPage = this.sizePageTable;
+
+  }
+
+  prevPage() {
+    this.endPage = this.startPage;
+    this.startPage = this.startPage - this.sizePageTable;
+  }
+
+  nextPage() {
+    this.startPage = this.endPage;
+    this.endPage = this.endPage + this.sizePageTable;
+  }
   
 }
