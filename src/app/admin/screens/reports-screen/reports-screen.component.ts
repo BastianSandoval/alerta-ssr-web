5cfc0b395c0bea756eccfd0309b254335be754f6
pablo-data <pablo.acu.ramirez@gmail.com>
2021-04-09 15:28:26 -0400

    feat(admin): add screen reports

---
+++ src/app/admin/screens/reports-screen/reports-screen.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reports-screen',
+  templateUrl: './reports-screen.component.html',
+  styleUrls: ['./reports-screen.component.css']
+})
+export class ReportsScreenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
