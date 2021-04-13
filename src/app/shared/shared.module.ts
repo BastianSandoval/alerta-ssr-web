8739a57b2a779c0b4aadfa4b570d797a4d884366
pablo-data <pablo.acu.ramirez@gmail.com>
2021-04-13 19:42:21 -0400

    feat(shared): add new filters on shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,18 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BreedRepeatPipe } from './pipes/breed-repeat/breed-repeat.pipe';
 import { BreedFilterPipe } from './pipes/breed-filter/breed-filter.pipe';
-import { DogFilterPipe } from './pipes/dog-filter/dog-filter.pipe';
+import { ReportFilterPipe } from './pipes/report-filter/report-filter.pipe';
+import { CategoryFilterPipe } from './pipes/category-filter/category-filter.pipe';
+import { DonationFilterPipe } from './pipes/donation-filter/donation-filter.pipe';
 
 
 @NgModule({
-  declarations: [BreedRepeatPipe, BreedFilterPipe, DogFilterPipe],
+  declarations: [BreedRepeatPipe, BreedFilterPipe, ReportFilterPipe, CategoryFilterPipe, DonationFilterPipe],
   imports: [
     CommonModule
   ],
   exports: [
     BreedRepeatPipe,
     BreedFilterPipe,
-    DogFilterPipe
+    ReportFilterPipe,
+    CategoryFilterPipe,
+    DonationFilterPipe
   ]
 })
 export class SharedModule { }
