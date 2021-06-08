8433e03d2e1b5de8cff5d6ac12b9303109c5025b
pablo-data <pablo.acu.ramirez@gmail.com>
2021-06-08 13:40:36 -0400

    refactor(admin): funtionality settings

--- src/app/admin/screens/settings-screen/settings-screen.component.ts
+++ src/app/admin/screens/settings-screen/settings-screen.component.ts
@@ -1,5 +1,10 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, FormGroupDirective, Validators } from '@angular/forms';
+import { FormService } from '../../../core/services/form/form.service';
+import { ConfigProviderService } from '../../../core/providers/config/config-provider.service';
+import { NotificationService } from '../../../core/services/notification/notification.service';
+import { ActivatedRoute } from '@angular/router';
+import { Config } from '../../../core/models/config.model';
 
 @Component({
   selector: 'app-settings-screen',
@@ -7,38 +12,112 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
   styleUrls: ['./settings-screen.component.css']
 })
 export class SettingsScreenComponent implements OnInit {
+
+  public id: string = '';
+  public config: Config[];
   
   checkoutForm: FormGroup;
   constructor(
-    private formBuilder: FormBuilder
+    private formService:FormService,
+    private configProviderService: ConfigProviderService,
+    private notificationService: NotificationService,
+    private route: ActivatedRoute,
     ){
 
-    this.checkoutForm = this.createFormGroup();
+    this.checkoutForm;
     this.createFormGroup();
 
     }
 
   ngOnInit() {
-
+    this.setReport();
   };
 
-  saveChange(event: Event){
-    event.preventDefault(); 
+  saveChange(event: Event, configForm: FormGroupDirective){
+    event.preventDefault();
+    if(configForm.valid){
+      console.log(this.checkoutForm.value);
+      this.submitConfig();
+    }
   }
 
   private createFormGroup() {
-    return new FormGroup({
-      actualLocation: new FormControl('', [Validators.required]),
-      otherLocation: new FormControl('',[Validators.required]),
-      minimalChecks: new FormControl('',[Validators.required]),
-      minimalRejections: new FormControl('',[Validators.required]),
+    this.checkoutForm = this.formService.buildFormGroup({
+      currentLocationRadius: new FormControl('', [Validators.required, Validators.pattern(/^-?(0|[1-9]\d*)?$/)]),
+      anotherLocationRadius: new FormControl('', [Validators.required, Validators.pattern(/^-?(0|[1-9]\d*)?$/)]),
+      minimalChecks: new FormControl('', [Validators.required, Validators.pattern(/^-?(0|[1-9]\d*)?$/)]),
+      minimalRejections: new FormControl('', [Validators.required, Validators.pattern(/^-?(0|[1-9]\d*)?$/)]),
     })
   }
 
-  get actualLocation() {return this.checkoutForm.get('actualLocation');}
-  get otherLocation() {return this.checkoutForm.get('otherLocation');}
-  get minimalChecks() {return this.checkoutForm.get('minimalChecks');}
-  get minimalRejections() {return this.checkoutForm.get('minimalRejections');}
+  public controlIsRequired(formControlName: string): boolean {
+    return this.formService.controlIsRequired(this.checkoutForm, formControlName);
+  }
 
+  public controlIsInvalid(formControlName: string): boolean {
+      return this.formService.controlIsInvalid(this.checkoutForm, formControlName);
+  }
+
+  public controlIsInvalidEmail(formControlName: string): boolean {
+      return this.formService.controlIsInvalidEmail(this.checkoutForm, formControlName);
+  }
+
+  public controlIsInvalidPattern(formControlName: string): boolean {
+    return this.formService.controlIsInvalidPattern(this.checkoutForm, formControlName);
+  }
+
+  public controlIsInvalidLength(formControlName: string): boolean {
+    return this.formService.controlIsInvalidLength(this.checkoutForm, formControlName);
+  }
+
+  public async submitConfig(): Promise<void> {
+    if (this.checkoutForm.valid) {
+      if (this.config[0]) {
+        await this.updateConfig();
+      } else {
+        await this.addConfig(this.checkoutForm);
+      }
+    }
+  }
+
+  public async addConfig(form: FormGroup): Promise<void> {
+    try {
+      await this.configProviderService.addConfig(this.checkoutForm.value).toPromise();
+      this.notificationService.success('La configuración ha sido creado');
+      this.checkoutForm.reset();
+    } catch (error) {
+      console.log(error);
+      this.notificationService.error('No se ha podido crear la configuración');
+    }
+  }
+
+  public async updateConfig(): Promise<void> {
+    try {
+      await this.configProviderService.updateConfig(this.config[0]._id, this.checkoutForm.value).toPromise();
+      this.notificationService.success('La configuración ha sido actualizado');
+    } catch (error) {
+      console.log(error);
+      this.notificationService.error('No se ha podido actualizar la configuración');
+    }
+  }
+
+
+  public async setReport(): Promise<void> {
+    try {
+        this.config = await this.configProviderService.getAllConfigs().toPromise();
+        if(this.config.length === 1){
+          this.checkoutForm.setValue({
+            currentLocationRadius: this.config[0].currentLocationRadius,
+            anotherLocationRadius: this.config[0].anotherLocationRadius,
+            minimalChecks: this.config[0].minimalChecks,            
+            minimalRejections: this.config[0].minimalRejections
+          });
+        }
+    }catch (error) {
+      console.log(error);
+      this.notificationService.error('No se ha podido cargar el producto');
+    }
+
+  }
 
 }
