1061461e0e3eae0daa685462495d4865195a700c
pablo-data <pablo.acu.ramirez@gmail.com>
2021-06-22 17:56:32 -0400

    feat(institution): add map and details report

--- src/app/institution/components/global-map/global-map.component.ts
+++ src/app/institution/components/global-map/global-map.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { NgModule } from '@angular/core';
-import { AgmCoreModule} from '@agm/core';
-import { PaisesService} from '@core/services/paises/paises.service'
-import { ActivatedRoute, Params} from '@angular/router';
 
+import { FormBuilder, FormControl, FormGroup, FormGroupDirective, Validators } from '@angular/forms';
+import { FormService } from '../../../core/services/form/form.service';
+
+import { ReportProviderService } from '../../../core/providers/report/report-provider.service';
+import { CommentProviderService } from '../../../core/providers/comment/comment-provider.service';
 
 @Component({
   selector: 'app-global-map',
@@ -12,43 +13,86 @@ import { ActivatedRoute, Params} from '@angular/router';
 })
 export class GlobalMapComponent implements OnInit {
 
- 
-  lat: number = -33.045560;
-  lng: number = -71.619420;
+  checkoutForm: FormGroup;
+  lat: number = -33;
+  lng: number = -71;
   zoom: number;
   mapTypeId:string;
   located:boolean;
   icon = "./../../../../assets/icons/place.svg";
 
-  paises: any[];
+  reportList: any[];
+  reporte: any;
+
+  comments: any[];
+
+  clickMark: boolean;
+  fitBound: boolean;
 
   constructor(
-    private paisesService:PaisesService,
-    private activatedRoute: ActivatedRoute,
+    private reportProviderService:ReportProviderService,
+    private commentProviderService: CommentProviderService,
+    private formService: FormService,
     ){
-	  this.zoom = 16;
-	  this.mapTypeId = 'hybrid';
+
+    this.createFormGroup();
+	  this.zoom = 5;
+	  this.mapTypeId = 'roadmap';
 	  this.located = false;
-	  this.paises = [];
+	  this.reportList = [];
+    this.clickMark = false;
+    this.fitBound = true;
   }
   
-  ngOnInit(){
-  // this.activatedRoute.params.subscribe((params: Params) => {
-  //   console.log(params);
-    
-  // });   
-	this.paises = this.paisesService.getAll();
-  console.log(this.paises);
-	this.getCurrentPosition();
+  async ngOnInit(){
+	let data:any = await this.reportProviderService.getAllReports().toPromise();
+  this.reportList = data.docs;
+  this.lat = -33.449125;
+  this.lng = -70.701529;
+  console.log(this.lat);
+  this.getCurrentPosition();
   
+  }
+
+  private createFormGroup(){
+    this.checkoutForm = this.formService.buildFormGroup({
+      comentario: new FormControl('',[Validators.required])
+    });
   }
 
   mapClicked(event:any){
 	  console.log(event);
+    this.clickMark = false;
   }
 
-  setMap(){
+  markerClicked(event:any){
+    let filterReport = this.reportList.filter(report => 
+      report.location.latitude == event.latitude && report.location.longitude == event.longitude
+    );
 
+    console.log(filterReport);
+    this.clickMark = true;
+    this.fitBound = false;
+    let latLng : any = {
+      lat: parseFloat(filterReport[0].location.latitude),
+      lng: parseFloat(filterReport[0].location.longitude),
+    }
+
+    this.reporte = filterReport[0];
+    console.log(this.reporte);
+    this.centerChange(latLng);
+    this.changeMapZoom(16);
+
+    this.comments = filterReport[0].comments;
+  }
+
+  changeMapZoom(event:any){
+    this.zoom = event;
+  }
+
+  centerChange(event: any){
+    this.lat = event.lat;
+    this.lng = event.lng;
   }
 
    /* mapClicked($event: any) {
@@ -64,6 +108,27 @@ export class GlobalMapComponent implements OnInit {
 		  this.lng = position.coords.longitude;
 		  this.located=true;
 	  })
+  }
+
+
+  public controlIsRequired(formControlName: string): boolean {
+    return this.formService.controlIsRequired(this.checkoutForm, formControlName);
+  }
+
+  public controlIsInvalid(formControlName: string): boolean {
+      return this.formService.controlIsInvalid(this.checkoutForm, formControlName);
+  }
+
+  public controlIsInvalidEmail(formControlName: string): boolean {
+      return this.formService.controlIsInvalidEmail(this.checkoutForm, formControlName);
+  }
+
+  public controlIsInvalidPattern(formControlName: string): boolean {
+    return this.formService.controlIsInvalidPattern(this.checkoutForm, formControlName);
+  }
+
+  public controlIsInvalidLength(formControlName: string): boolean {
+    return this.formService.controlIsInvalidLength(this.checkoutForm, formControlName);
   }
   
 
