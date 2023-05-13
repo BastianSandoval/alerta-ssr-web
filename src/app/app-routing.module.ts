c92dd6576e492c832c2f91fe2c94a6ec25bb59a1
BastianSandoval <demosandobal@gmail.com>
2023-05-13 08:32:29 -0400

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'visitor',
+    redirectTo: 'institution',
     pathMatch: 'full'
   },
   { 
